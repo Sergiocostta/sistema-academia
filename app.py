4070c6b8a2b36e370910172387cfb31f7a38a216
Sergiocostta <sergiofilhoal@gmail.com>
2025-05-25 21:23:14 -0300

    projeto inicial

---
+++ app.py
@@ -0,0 +1,67 @@
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from treino_predefinido import treinos_prontos
+import json
+
+app = Flask(__name__)
+app.secret_key = 'segredo'
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+        tipo = request.form['tipo']
+        usuarios = carregar_usuarios()
+        for u in usuarios:
+            if u['usuario'] == usuario:
+                return render_template("cadastro.html", erro="Usuário já existe.")
+        usuarios.append({'usuario': usuario, 'senha': senha, 'tipo': tipo})
+        with open("usuarios.json", "w") as f:
+            json.dump(usuarios, f, indent=2)
+        return redirect(url_for('login'))
+    return render_template("cadastro.html")
+
+def carregar_usuarios():
+    try:
+        with open("usuarios.json", "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        usuarios = carregar_usuarios()
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+        for u in usuarios:
+            if u['usuario'] == usuario and u['senha'] == senha:
+                session['usuario'] = usuario
+                session['tipo'] = u['tipo']
+                return redirect(url_for('painel'))
+        return render_template("login.html", erro="Usuário ou senha incorretos")
+    return render_template("login.html")
+
+@app.route('/painel')
+def painel():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    if session['tipo'] == 'admin':
+        return render_template("painel_admin.html", usuario=session['usuario'])
+    else:
+        return redirect(url_for('mostrar_treinos'))
+
+@app.route('/treinos')
+def mostrar_treinos():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    return render_template('treinos.html', treinos=treinos_prontos)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ treino_predefinido.py
@@ -0,0 +1,15 @@
+
+treinos_prontos = [
+    {
+        "titulo": "Treino de Peito",
+        "descricao": "Supino, Crucifixo, Flexão",
+        "video": "https://www.youtube.com/embed/UBMk30rjy0o",
+        "imagem": "https://via.placeholder.com/150"
+    },
+    {
+        "titulo": "Treino de Costas",
+        "descricao": "Puxada, Remada, Peso morto",
+        "video": "https://www.youtube.com/embed/fqzJWGQNxUM",
+        "imagem": "https://via.placeholder.com/150"
+    }
+]
